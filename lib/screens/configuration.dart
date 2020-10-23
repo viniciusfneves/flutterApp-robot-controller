ad1e2d2e2c76fdb278e83b03d0f6fa5fdd5351a6
Vinícius <viniciusfneves@poli.ufrj.br>
2020-10-23 15:25:07 -0300

    Styling Configurations page

--- lib/screens/configuration.dart
+++ lib/screens/configuration.dart
@@ -12,12 +12,89 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
   @override
   Widget build(BuildContext context) {
     return ScreenBase(
-      bodyWidget: Center(
-          child: Column(
+      bodyWidget: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text('Configuration'),
+          // This container will be the chat with the robot
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: (MediaQuery.of(context).size.height - 100) * 0.4,
+            decoration: BoxDecoration(
+              color: Colors.lightGreen[100],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(12),
+                bottomRight: Radius.circular(12),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                "Chat",
+                style: TextStyle(fontSize: 60),
+              ),
+            ),
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          // Buttons
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: (MediaQuery.of(context).size.height - 440),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  buttonRow(),
+                  buttonRow(),
+                  buttonRow(),
+                ],
+              ),
+            ),
+          )
         ],
-      )),
+      ),
     );
   }
+}
+
+// Gambiarra para exibir vários ícones
+// TO-DO!  criar uma classe separada para exibir cada botão
+
+Widget buttonRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      RaisedButton(
+        color: Colors.red[600],
+        onPressed: () {},
+        child: Text(
+          'Function 1',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      RaisedButton(
+        color: Colors.red[600],
+        onPressed: () {},
+        child: Text(
+          'Function 2',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      RaisedButton(
+        color: Colors.red[600],
+        onPressed: () {},
+        child: Text(
+          'Function 3',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ],
+  );
 }
