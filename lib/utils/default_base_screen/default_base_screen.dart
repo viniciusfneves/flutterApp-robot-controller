9a37968d67ecbf6363c35a001a98ab4fc0f6696d
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2020-10-28 03:20:06 -0300

    Transforming Widget in Stateless and adding button for activate DarkMode

--- lib/utils/default_base_screen/default_base_screen.dart
+++ lib/utils/default_base_screen/default_base_screen.dart
@@ -2,6 +2,7 @@ import 'package:blue_app/utils/app_routes.dart';
 import 'package:blue_app/utils/theme_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'drawer_links.dart';
 
@@ -9,17 +10,11 @@ import 'drawer_links.dart';
 // seguido por esse aplicativo. Recebe como parâmetro um Widget em
 // bodyWidget que será adicionado como corpo da página criada
 
-class ScreenBase extends StatefulWidget {
+class ScreenBase extends StatelessWidget {
   @required
   final Widget bodyWidget;
 
-  const ScreenBase({Key key, this.bodyWidget}) : super(key: key);
-
-  @override
-  ScreenBaseState createState() => ScreenBaseState();
-}
-
-class ScreenBaseState extends State<ScreenBase> {
+  ScreenBase({Key key, this.bodyWidget}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,43 +23,59 @@ class ScreenBaseState extends State<ScreenBase> {
         centerTitle: true,
       ),
       drawer: Drawer(
-        child: ListView(children: <Widget>[
-          DrawerHeader(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Image.asset(
-                  'lib/assets/images/minerva_logo.jpg',
-                  height: 80,
-                ),
-                Text(
-                  'Minerva Bluetooth App',
-                  style: TextStyle(fontSize: 16),
-                  overflow: TextOverflow.fade,
-                  softWrap: true,
-                ),
-              ],
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset(
+                    'lib/assets/images/minerva_logo.jpg',
+                    height: 80,
+                  ),
+                  Text(
+                    'Minerva Bluetooth App',
+                    style: TextStyle(fontSize: 16),
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: true,
+                  ),
+                ],
+              ),
             ),
-          ),
-          DrawerLink(
-            icon: Icons.bluetooth,
-            title: 'Conections',
-            pageRoute: AppRoutes.Connection,
-          ),
-          DrawerLink(
-            icon: Icons.account_tree_outlined,
-            title: 'Configuration',
-            pageRoute: AppRoutes.Configuration,
-          ),
-          DrawerLink(
-            icon: Icons.blur_circular,
-            title: 'Controls',
-            pageRoute: AppRoutes.Control,
-          ),
-          ThemeController(),
-        ]),
+            DrawerLink(
+              icon: Icons.bluetooth,
+              title: 'Conections',
+              pageRoute: AppRoutes.Connection,
+            ),
+            DrawerLink(
+              icon: Icons.account_tree_outlined,
+              title: 'Configuration',
+              pageRoute: AppRoutes.Configuration,
+            ),
+            DrawerLink(
+              icon: Icons.blur_circular,
+              title: 'Controls',
+              pageRoute: AppRoutes.Control,
+            ),
+            Consumer(
+              builder: (BuildContext context, ThemeController controller,
+                  Widget child) {
+                return ListTile(
+                  title: Text('Dark Mode'),
+                  leading: Icon(Icons.brightness_2),
+                  trailing: Switch(
+                    value: Provider.of<ThemeController>(context).isDarkMode,
+                    onChanged: (value) {
+                      controller.changeTheme();
+                    },
+                  ),
+                );
+              },
+            )
+          ],
+        ),
       ),
-      body: this.widget.bodyWidget,
+      body: this.bodyWidget,
     );
   }
 }
