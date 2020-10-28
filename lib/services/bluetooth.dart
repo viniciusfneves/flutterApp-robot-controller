b5912f2a8b800e8531ed41e8061ed40974aac876
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2020-10-28 03:35:12 -0300

    Bluetooth scan mode is now set to balanced
    Implementing for tests. If needed, can be changed to lowLatency.

--- lib/services/bluetooth.dart
+++ lib/services/bluetooth.dart
@@ -21,7 +21,7 @@ class Bluetooth extends StatefulWidget {
         }
       },
     );
-    await flutterBlue.startScan(timeout: Duration(seconds: 10));
+    await flutterBlue.startScan(timeout: Duration(seconds: 10), scanMode: ScanMode.balanced);
     _deviceStream.close();
   }
 }
