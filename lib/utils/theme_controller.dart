8426ac7cc66a7ec4e32138083029bf96842ccb61
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2020-10-28 03:17:40 -0300

    ThemeController is no longer a Widget and now extends ChangeNotifier
    Also, new functions for controlling the theme states were created

--- lib/utils/theme_controller.dart
+++ lib/utils/theme_controller.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ThemeController extends StatefulWidget {
-  @override
-  _ThemeControllerState createState() => _ThemeControllerState();
-}
+class ThemeController extends ChangeNotifier{
+  bool _isDarkTheme = false;
+  bool get isDarkMode => _isDarkTheme; 
 
-class _ThemeControllerState extends State<ThemeController> {
-  bool _isDarkThemeActive = false;
-
-  void changeTheme(bool currentTheme) {
-    _isDarkThemeActive = !currentTheme;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        'Dark Mode',
-        style: TextStyle(fontSize: 15),
-      ),
-      leading: Icon(
-        Icons.brightness_2,
-        size: 22,
-      ),
-      trailing: Switch(
-        value: _isDarkThemeActive,
-        onChanged: (bool value) {
-          setState(() {
-            changeTheme(_isDarkThemeActive);
-          });
-        },
-      ),
-    );
+  changeTheme(){
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
   }
 }
+
 
               
