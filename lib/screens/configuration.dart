9a61f276a1665e7ac24a764227fb01e4ecd0de2c
Vinícius F. Neves <viniciusfneves@poli.ufrj.br>
2020-10-30 03:42:00 -0300

    Removing unnecessary widgets & changing some functions and some comments
    Also changing some styles in between xD

--- lib/screens/configuration.dart
+++ lib/screens/configuration.dart
@@ -1,6 +1,8 @@
 import 'package:blue_app/utils/default_base_screen/default_base_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:blue_app/services/bluetooth.dart';
+import 'package:blue_app/utils/theme_controller.dart';
+import 'package:provider/provider.dart';
 
 class ConfigurationPage extends StatefulWidget {
   @override
@@ -15,7 +17,7 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
       bodyWidget: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          // This container will be the chat with the robot
+          // Chat
           Flexible(
             flex: 5,
             child: Container(
@@ -35,26 +37,22 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
               ),
             ),
           ),
+          // Divider
           Divider(
-            thickness: 1,
+            color: Color.fromARGB(255, 160, 0, 5),
           ),
           // Buttons
           Flexible(
             flex: 6,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: (MediaQuery.of(context).size.height - 440),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                  ),
-                ),
-                child: buttons(context, 24),
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                standardButtons(context),
+                makeMultipleButtons(context, 16)
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
@@ -64,24 +62,44 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
 // Gambiarra para exibir vários ícones
 // TO-DO!  criar uma classe separada para exibir cada botão
 
-Widget buttons(BuildContext context, int quantity) {
+Widget button(BuildContext context, String title, {int qtd = 4}) {
+  double containerWidth;
+  if (qtd == 2) {
+    containerWidth = 2.07;
+  } else {
+    containerWidth = qtd + 0.25;
+  }
+  return Container(
+    width: MediaQuery.of(context).size.width / containerWidth,
+    child: RaisedButton(
+      color: Provider.of<ThemeController>(context).isDarkMode
+          ? Color.fromARGB(200, 160, 0, 5)
+          : Colors.red[900],
+      onPressed: () {},
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.white),
+        overflow: TextOverflow.fade,
+      ),
+    ),
+  );
+}
+
+Widget standardButtons(BuildContext context) {
+  return Wrap(
+    alignment: WrapAlignment.spaceAround,
+    children: [
+      button(context, 'Ligar', qtd: 2),
+      button(context, 'Desligar', qtd: 2)
+    ],
+  );
+}
+
+Widget makeMultipleButtons(BuildContext context, int quantity) {
   List<Widget> buttons = new List<Widget>();
   for (int i = 1; i <= (quantity); i++) {
-    buttons.add(
-      Container(
-        width: MediaQuery.of(context).size.width / 4.22,
-        child: RaisedButton(
-          color: Colors.red[600],
-          onPressed: () {},
-          child: Text(
-            'Função $i',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white),
-            overflow: TextOverflow.fade,
-          ),
-        ),
-      ),
-    );
+    buttons.add(button(context, "Função $i"));
   }
   return Wrap(
     alignment: WrapAlignment.spaceAround,
