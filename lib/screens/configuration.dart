c68eb0fcf4e4d90a2ca20b55c137fad9e3538dab
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-09-21 16:58:52 -0300

    Estruturando página de controle remoto e página de configurações
    Inicio da página de configurações
    
    
    Início da página de controle remoto

--- lib/screens/configuration.dart
+++ lib/screens/configuration.dart
@@ -1,24 +1,105 @@
-import 'package:blue_app/screens/screen_template/screen_template.dart';
+import 'package:blue_app/communications/WiFi/websocket_handler.dart';
+import 'package:blue_app/providers/widget_states_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:blue_app/screen_template/screen_template.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ConfigurationPage extends StatefulWidget {
-  @override
-  _ConfigurationPageState createState() => _ConfigurationPageState();
-}
-
-class _ConfigurationPageState extends State<ConfigurationPage> {
+class ConfigurationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenBase(
       bodyWidget: Center(
-        child: Text(
-          'Configurações',
-          style: TextStyle(
-            fontSize: 20,
+          child: Column(
+        children: [
+          // Display de estado
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 32,
+                height: 32,
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: context.watch<RobotWidgetState>().readystate),
+              ),
+              Container(
+                width: 32,
+                height: 32,
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: context.watch<RobotWidgetState>().fightstate),
+              ),
+              Container(
+                width: 32,
+                height: 32,
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: context.watch<RobotWidgetState>().disabledstate),
+              )
+            ],
           ),
-        ),
-      ),
+
+          // Botões de controle de estado
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 120,
+                height: 80,
+                child: ElevatedButton(
+                    child: Text(
+                      "Start",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    style: getStyle(Color.fromARGB(255, 25, 150, 25)),
+                    onPressed: () {
+                      websocket.sink.add('{"event_request": "start"}');
+                      context.read<RobotWidgetState>().setFightState();
+                    }),
+              ),
+              Container(
+                width: 135,
+                height: 80,
+                child: ElevatedButton(
+                    child: Text(
+                      "Disengage",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    style: getStyle(Color.fromARGB(255, 200, 25, 25)),
+                    onPressed: () {
+                      websocket.sink.add('{"event_request": "terminate"}');
+                      context.read<RobotWidgetState>().setDisabledState();
+                    }),
+              ),
+              Container(
+                width: 90,
+                height: 80,
+                child: ElevatedButton(
+                    child: Text(
+                      "Arm",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    style: getStyle(Color.fromARGB(255, 190, 150, 0)),
+                    onPressed: () {
+                      websocket.sink.add('{"event_request": "reset"}');
+                      context.read<RobotWidgetState>().setReadyState();
+                    }),
+              ),
+            ],
+          ),
+        ],
+      )),
     );
   }
+}
+
+ButtonStyle getStyle(Color color) {
+  return ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(color),
+      padding:
+          MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(18)));
 }
--- lib/screens/control.dart
+++ lib/screens/control.dart
@@ -1,17 +1,74 @@
-import 'package:blue_app/screens/screen_template/screen_template.dart';
+import 'dart:convert';
+import 'package:blue_app/communications/wifi/websocket_handler.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:provider/provider.dart';
+import 'package:blue_app/providers/robot_data_provider.dart';
+import 'package:blue_app/communications/WiFi/http_requests.dart';
+import 'package:blue_app/screen_template/screen_template.dart';
+import 'package:control_pad/control_pad.dart';
 
-class ControlPage extends StatelessWidget {
+class ControlPage extends StatefulWidget {
+  ControlPage({Key key}) : super(key: key);
+
+  @override
+  State<ControlPage> createState() => _ControlPageState();
+}
+
+class _ControlPageState extends State<ControlPage> {
+  void initState() {
+    super.initState();
+    websocket.stream.listen((data) {
+      Map<String, dynamic> json = jsonDecode(data);
+      if (json.containsKey('configurations')) {
+        print(json['configurations']);
+      }
+    });
+  }
+
+  double valor = 50;
+
   @override
   Widget build(BuildContext context) {
     return ScreenBase(
-      bodyWidget: Center(
-        child: Text(
-          'Controle',
-          style: TextStyle(
-            fontSize: 20,
+      bodyWidget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Velocidade Máxima",
+            style: TextStyle(fontSize: 24),
           ),
-        ),
+          Container(
+            height: 8,
+          ),
+          Text(
+            "${(context.watch<RobotData>().maxSpeed / 255 * 100).toStringAsFixed(1)} %",
+            style: TextStyle(fontSize: 24),
+          ),
+          Slider(
+            min: 35,
+            max: 255,
+            value: valor,
+            onChanged: (newValue) {
+              setState(() {
+                valor = newValue;
+              });
+            },
+          ),
+          Container(
+            height: 55,
+          ),
+          JoystickView(
+            size: 320,
+            backgroundColor: Colors.black38,
+            innerCircleColor: Colors.black45,
+            interval: Duration(milliseconds: 500),
+            onDirectionChanged: (degrees, intensity) {
+              double rad = degrees * pi / 180;
+              sendControllerCommand(intensity * cos(rad), intensity * sin(rad));
+            },
+          ),
+        ],
       ),
     );
   }
