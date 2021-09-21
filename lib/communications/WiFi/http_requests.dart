2e1f6318a6ac3f1262bc50452a29c37fee228a7e
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-09-21 16:59:12 -0300

    Conectando app via http e websocket ao robô

--- lib/communications/WiFi/http_requests.dart
+++ lib/communications/WiFi/http_requests.dart
@@ -1,1 +1,16 @@
 import 'package:http/http.dart' as http;
+
+void sendControllerCommand(double linear, double angular) async {
+  final response = await http.post(Uri.parse('http://192.168.4.1/controller'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: '{"controller":{"linearSpeed":$linear,"angularSpeed":$angular}}');
+
+  if (response.statusCode == 200) {
+    // Se o server enviar status 200, então tudo OK
+  } else {
+    // Se o server enviar status 200, então houve algum erro
+    print("bad");
+  }
+}
--- lib/communications/WiFi/websocket_handler.dart
+++ lib/communications/WiFi/websocket_handler.dart
@@ -1,1 +1,5 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
+
+final websocket = WebSocketChannel.connect(
+  Uri.parse('ws://192.168.4.1:81'),
+);
