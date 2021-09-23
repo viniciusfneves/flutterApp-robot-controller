404b77f45aad2021ced45d0690061aa6a551932f
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-09-23 16:58:59 -0300

    Expondo broadcast stream das mensagens enviadas pelo robô via websocket

--- lib/communications/WiFi/websocket_handler.dart
+++ lib/communications/WiFi/websocket_handler.dart
@@ -3,3 +3,5 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 final websocket = WebSocketChannel.connect(
   Uri.parse('ws://192.168.4.1:81'),
 );
+
+final websocketData = websocket.stream.asBroadcastStream();
