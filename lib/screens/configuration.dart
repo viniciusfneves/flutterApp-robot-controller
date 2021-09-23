264de99d95682bab1eb5d17c137e2bbe640f7e4a
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-09-23 17:07:44 -0300

    Página de configurações funcional
    - Lê mensagens do websocket e passa para o JsonHandler do código
    - Atualiza a interface gráfica de acordo sempre que há uma nova mudança nas configurações do robô
    - Envia comandos para o robô para controle do estado da luta e configurações de estratégia inicial, de busca e de perseguição
    - Funções reutilizáveis para criação e atualização dos widgets, deixando o entendimento da página mais simples e fácil de alterar o design
    - Pronta para receber atualizações de estratégias, caso haja overflow da página, se torna um Widget rolável automaticamente para permitir a visualização de toda a página

--- lib/screens/configuration.dart
+++ lib/screens/configuration.dart
@@ -1,105 +1,225 @@
+import 'dart:convert';
 import 'package:blue_app/communications/WiFi/websocket_handler.dart';
+import 'package:blue_app/communications/json/json_handler.dart';
+import 'package:blue_app/providers/robot_data_provider.dart';
 import 'package:blue_app/providers/widget_state_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:blue_app/screen_template/screen_template.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ConfigurationPage extends StatelessWidget {
+class ConfigurationPage extends StatefulWidget {
+  @override
+  State<ConfigurationPage> createState() => _ConfigurationPageState();
+}
+
+class _ConfigurationPageState extends State<ConfigurationPage> {
+  void initState() {
+    websocketData.listen((data) {
+      Map<String, dynamic> json = jsonDecode(data);
+      processJsonMessage(context, json);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenBase(
-      bodyWidget: Center(
-          child: Column(
+      bodyWidget: Column(
         children: [
-          // Display de estado
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                width: 32,
-                height: 32,
-                margin: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: context.watch<WidgetState>().readystate),
-              ),
-              Container(
-                width: 32,
-                height: 32,
-                margin: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: context.watch<WidgetState>().fightstate),
-              ),
-              Container(
-                width: 32,
-                height: 32,
-                margin: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: context.watch<WidgetState>().disabledstate),
-              )
-            ],
-          ),
-
-          // Botões de controle de estado
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                width: 120,
-                height: 80,
-                child: ElevatedButton(
-                    child: Text(
-                      "Start",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+          robotStateConfigurationWidgets(context),
+          Flexible(
+            child: SingleChildScrollView(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: [
+                    ...robotStrategyConfigurationWidgets(
+                      context,
+                      "Modo de Operação",
+                      "mode",
+                      ["Auto", "RC"],
+                      context.watch<RobotData>().modeConfiguration,
                     ),
-                    style: getStyle(Color.fromARGB(255, 25, 150, 25)),
-                    onPressed: () {
-                      websocket.sink.add('{"event_request": "start"}');
-                      context.read<WidgetState>().setFightState();
-                    }),
-              ),
-              Container(
-                width: 135,
-                height: 80,
-                child: ElevatedButton(
-                    child: Text(
-                      "Disengage",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ...robotStrategyConfigurationWidgets(
+                      context,
+                      "Movimento Inicial",
+                      "initial",
+                      ["None", "Full Frente", "Zig Zag"],
+                      context.watch<RobotData>().initialMoveConfiguration,
                     ),
-                    style: getStyle(Color.fromARGB(255, 200, 25, 25)),
-                    onPressed: () {
-                      websocket.sink.add('{"event_request": "terminate"}');
-                      context.read<WidgetState>().setDisabledState();
-                    }),
-              ),
-              Container(
-                width: 90,
-                height: 80,
-                child: ElevatedButton(
-                    child: Text(
-                      "Arm",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ...robotStrategyConfigurationWidgets(
+                      context,
+                      "Busca",
+                      "search",
+                      ["None", "Radar"],
+                      context.watch<RobotData>().searchConfiguration,
                     ),
-                    style: getStyle(Color.fromARGB(255, 190, 150, 0)),
-                    onPressed: () {
-                      websocket.sink.add('{"event_request": "reset"}');
-                      context.read<WidgetState>().setReadyState();
-                    }),
+                    ...robotStrategyConfigurationWidgets(
+                      context,
+                      "Perseguição",
+                      "chase",
+                      ["Standard"],
+                      context.watch<RobotData>().chaseConfiguration,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    )
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
         ],
-      )),
+      ),
     );
   }
 }
 
-ButtonStyle getStyle(Color color) {
-  return ButtonStyle(
-      backgroundColor: MaterialStateProperty.all<Color>(color),
-      padding:
-          MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(18)));
+Widget robotStateConfigurationWidgets(BuildContext context) {
+  List<Widget> widgetsToDisplay = [
+    // Display de estado
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 32,
+          height: 32,
+          margin: EdgeInsets.only(bottom: 4, top: 8, left: 20, right: 14),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: context.watch<WidgetState>().robotStateCircle,
+          ),
+        ),
+        Container(
+          width: 85,
+          child: Text(
+            context.watch<WidgetState>().robotStateText,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    ),
+    Divider(thickness: 2),
+    // Botões de controle de estado
+    Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      alignment: WrapAlignment.spaceAround,
+      children: [
+        makeEventRequestButton("Start", "start", Colors.green[800]),
+        makeEventRequestButton("Disengage", "terminate", Colors.red[900]),
+        makeEventRequestButton("Arm", "reset", Colors.lime[800]),
+      ],
+    ),
+    SizedBox(
+      height: 8,
+    )
+  ];
+
+  return Padding(
+    padding: EdgeInsets.only(top: 6),
+    child: Column(
+      children: [...widgetsToDisplay],
+    ),
+  );
+}
+
+List<Widget> robotStrategyConfigurationWidgets(
+  BuildContext context,
+  String textInidication,
+  String typeOfStrategy,
+  List<String> availableStrategys,
+  String selectedStrategy,
+) {
+  List<Widget> strategyButtons = [];
+
+  availableStrategys.forEach(
+    (strategyName) {
+      if (strategyName.replaceAll(RegExp(' '), '_').toLowerCase() !=
+          selectedStrategy) {
+        strategyButtons.add(makeStrategyRequestButton(
+            strategyName, typeOfStrategy, Colors.blueGrey));
+      } else {
+        strategyButtons.add(makeStrategyRequestButton(
+            strategyName, typeOfStrategy, Colors.deepPurple[800]));
+      }
+    },
+  );
+
+  return [
+    Padding(
+      padding: const EdgeInsets.only(top: 8, bottom: 6),
+      child: Text(
+        textInidication,
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+        ),
+      ),
+    ),
+    Wrap(
+      alignment: WrapAlignment.center,
+      spacing: 14,
+      runSpacing: 14,
+      children: [...strategyButtons],
+    )
+  ];
+}
+
+Widget makeEventRequestButton(
+  String displayText,
+  String requestText,
+  Color buttonColor,
+) {
+  return Container(
+    width: 165,
+    height: 80,
+    child: ElevatedButton(
+      child: Text(
+        displayText,
+        style: TextStyle(
+            fontSize: 19, color: Colors.white, overflow: TextOverflow.ellipsis),
+      ),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              EdgeInsets.all(18))),
+      onPressed: () {
+        websocket.sink.add('{"event_request":"${requestText.toLowerCase()}"}');
+      },
+    ),
+  );
+}
+
+Widget makeStrategyRequestButton(
+  String displayText,
+  String strategyType,
+  Color buttonColor,
+) {
+  return Container(
+    width: 112,
+    height: 62,
+    child: ElevatedButton(
+      child: Text(
+        displayText,
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+        ),
+        overflow: TextOverflow.ellipsis,
+      ),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              EdgeInsets.all(18))),
+      onPressed: () {
+        websocket.sink.add(
+            '{"${strategyType.toLowerCase()}":"${displayText.replaceAll(RegExp(' '), '_').toLowerCase()}"}');
+      },
+    ),
+  );
 }
