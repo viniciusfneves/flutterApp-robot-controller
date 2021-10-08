3f87294aeeba126209f3bd1004ccd6a674b24671
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-10-08 09:11:40 -0300

    Atualizando para novo formato JSON de comunicação

--- lib/communications/json/json_handler.dart
+++ lib/communications/json/json_handler.dart
@@ -4,12 +4,30 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void processJsonMessage(BuildContext context, Map<String, dynamic> json) {
-  if (json.containsKey("robot_name")) {
-    context.read<WidgetState>().addRobotNameToAppTitle(json["robot_name"]);
+  if (json.containsKey("info")) {
+    context.read<WidgetState>().addRobotNameToAppTitle(json["info"]["robot_name"]);
+    context.read<RobotData>().setAvailableOP(json["info"]["available_opponent_sensors"]);
+    context.read<RobotData>().setAvailableEdge(json["info"]["available_edge_sensors"]);
   }
 
-  if (json.containsKey("robot_status")) {
-    switch (json["robot_status"]) {
+  if (json.containsKey("configurations")) {
+    context.read<RobotData>().setStartTime(json["configurations"]["start_time"]);
+    context.read<RobotData>().setMaxSpeed(json["configurations"]["max_speed"]);
+    context.read<RobotData>().setPID(
+        json["configurations"]["pid"]["kp"].toDouble(),
+        json["configurations"]["pid"]["ki"].toDouble(),
+        json["configurations"]["pid"]["kd"].toDouble());
+    context.read<RobotData>().setMode(json["configurations"]["mode"]);
+    context.read<RobotData>().setInitialMove(json["configurations"]["initial_move"]);
+    context.read<RobotData>().setSearch(json["configurations"]["search"]);
+    context.read<RobotData>().setChase(json["configurations"]["chase"]);
+  }
+
+  if (json.containsKey("readings")) {
+    context.read<RobotData>().setOpponentSensors(json["readings"]["opponent"]);
+    context.read<RobotData>().setEdgeSensors(json["readings"]["edge"]);
+    context.read<RobotData>().setWheelPower(json["readings"]["motor"][0], json["readings"]["motor"][1]);
+    switch (json["readings"]["robot_status"]) {
       case "ready":
         context.read<WidgetState>().setReadyState();
         break;
@@ -23,43 +41,5 @@ void processJsonMessage(BuildContext context, Map<String, dynamic> json) {
         context.read<WidgetState>().setFightState();
         break;
     }
-  }
-
-  // ["available_opponent_sensors"] ["available_edge_sensors"]
-
-  if (json.containsKey("available_opponent_sensors")) {
-    context
-        .read<RobotData>()
-        .setAvailableOP(json["available_opponent_sensors"]);
-  }
-
-  if (json.containsKey("available_edge_sensors")) {
-    context.read<RobotData>().setAvailableEdge(json["available_edge_sensors"]);
-  }
-
-  if (json.containsKey("configurations")) {
-    context
-        .read<RobotData>()
-        .setStartTime(json["configurations"]["settings"]["start_time"]);
-    context
-        .read<RobotData>()
-        .setMaxSpeed(json["configurations"]["settings"]["max_speed"]);
-    context.read<RobotData>().setPID(
-        json["configurations"]["settings"]["pid"]["kp"].toDouble(),
-        json["configurations"]["settings"]["pid"]["ki"].toDouble(),
-        json["configurations"]["settings"]["pid"]["kd"].toDouble());
-    context.read<RobotData>().setMode(json["configurations"]["mode"]);
-    context
-        .read<RobotData>()
-        .setInitialMove(json["configurations"]["initial_move"]);
-    context.read<RobotData>().setSearch(json["configurations"]["search"]);
-    context.read<RobotData>().setChase(json["configurations"]["chase"]);
-  }
-
-  if (json.containsKey("readings")) {
-    context.read<RobotData>().setOpponentSensors(json["readings"]["opponent"]);
-    context.read<RobotData>().setEdgeSensors(json["readings"]["edge"]);
-    context.read<RobotData>().setWheelPower(
-        json["readings"]["motors"][0], json["readings"]["motors"][1]);
   }
 }
