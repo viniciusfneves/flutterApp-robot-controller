f0a10b0c64dbb6f446cf5e4ae0821a9d1ad42dfb
Vinicius Ferreira Neves <viniciusfneves@poli.ufrj.br>
2022-03-02 13:48:04 -0300

    Providers de controle do estado do app

--- lib/json/json_handler.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-void processJsonMessage(BuildContext ctx, Map<String, dynamic> json) {
-  // TODO: Criar toda a lógica de processamento da mensagem JSON: info, configs e readings
-  // TODO: Atualizar os providers com as informações recebidas
-}
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,4 +1,40 @@
+import 'dart:convert';
+
+import 'package:blue_app/data/robot_configs.dart';
 import 'package:blue_app/routes/app_routes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
+// Providers para decisões do aplicativo
 final selectedPage = StateProvider<AppRoutes>((_) => AppRoutes.configuration);
+final themeIsDark = StateProvider<bool>((_) => false);
+
+// Providers para conexões
+final wsAdress = StateProvider<String>((_) => "");
+final ws = StateProvider<WebSocketChannel>(
+  (ref) {
+    final adress = ref.watch(wsAdress);
+    return WebSocketChannel.connect(Uri.parse(adress));
+  },
+);
+
+final robotDataStream = StreamProvider<Map<String, dynamic>>(
+  (ref) async* {
+    final connection = ref.watch(ws);
+
+    await for (final message in connection.stream) {
+      Map<String, dynamic> json;
+      json = jsonDecode(message as String) as Map<String, dynamic>;
+      yield json;
+    }
+  },
+);
+
+final robotConfig = StateProvider<RobotConfigs>(
+  (ref) => RobotConfigs(
+    mode: "",
+    initial: "",
+    search: "",
+    chase: "",
+  ),
+);
