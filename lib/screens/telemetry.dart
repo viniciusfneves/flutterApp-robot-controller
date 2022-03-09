d44702a87f9de35b84ca686425500b6fbf803cc4
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2022-03-09 01:25:50 -0300

    Iniciando processo de display da telemetria do motor

--- lib/screens/telemetry.dart
+++ lib/screens/telemetry.dart
@@ -1,5 +1,6 @@
 import 'package:blue_app/providers/providers.dart';
 import 'package:blue_app/style/colors.dart';
+import 'package:blue_app/style/texts.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -52,9 +53,65 @@ class TelemetryPage extends StatelessWidget {
                       ],
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 75,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Motor(),
+                        MotorDisplay(),
+                        MotorDisplay(),
+                        Motor(),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MotorDisplay extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Padding(
+      padding: EdgeInsets.all(24.0),
+      child: GeneralPurposeText(
+        "255",
+        adaptiveColor: false,
+        color: AppColors.standardAmbar,
+        fontSize: 32,
+      ),
+    );
+  }
+}
+
+class Motor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
+        width: 72,
+        height: 120,
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [
+              Colors.purple,
+              Colors.deepPurple,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(
+            15,
           ),
         ),
       ),
